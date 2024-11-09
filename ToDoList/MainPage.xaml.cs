f80ccf12e83510877b1467f27d743813d500dbcd
AlexPC <AlexPC@ALEX_PC>
2024-11-09 10:35:49 +0500

    Добавьте файлы проекта.

---
+++ ToDoList/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace ToDoList
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new TodoViewModel();
+        }
+    }
+}
